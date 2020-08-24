eaefddd727f9571ccb0076f6a8eaa421efd527a6
Linus Romland (linusromland) <48645689+linusromland@users.noreply.github.com>
2020-08-24 17:02:08 +0200

    Added C# Windows Form Project. Hello Niklas
    Created C# Windows Form Project.
    Created a Hello World in both Console and Windows Form.

---
+++ Vecka 35/Hello World/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_World
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Hello Niklas!";
+            Console.WriteLine("Hello Niklas!");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
