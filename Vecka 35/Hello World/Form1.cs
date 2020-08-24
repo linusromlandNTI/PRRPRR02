98df453bc01af7b8cdebcfeafe6780603f83c01b
Linus Romland (linusromland) <48645689+linusromland@users.noreply.github.com>
2020-08-24 17:16:42 +0200

    Moved the int. I am stupid. Works Now

--- Vecka 35/Hello World/Form1.cs
+++ Vecka 35/Hello World/Form1.cs
@@ -12,43 +12,42 @@ namespace Hello_World
 {
     public partial class Form1 : Form
     {
+
+        int x = 0;
+
         public Form1()
         {
             InitializeComponent();
             label1.Text = "Hello Niklas!";
             Console.WriteLine("Hello Niklas!");
+
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-            int x = 0;
             switch (x){
                 case 0:
                     label1.Text = "Why did you press on me?";
-                    x++;
                     break;
                 case 1:
                     label1.Text = "AGAIN?? WHYY SOME MEAN??";
-                    x++;
                     break;
                 case 2:
                     label1.Text = "I hate you!";
-                    x++;
                     break;
                 case 3:
                     label1.Text = "Go and die";
-                    x++;
                     break;
                 case 4:
                     label1.Text = "I am bored okay?";
-                    x++;
                     break;
                 case 5:
                     label1.Text = "Hello Niklas!";
-                    x++;
                     break;
             }
-            
+            x++;
+
+
         }
     }
 }
