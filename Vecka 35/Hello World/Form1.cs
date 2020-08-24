4a99da07ff4029a993920d37f23f573d4d0d667d
Linus Romland (linusromland) <48645689+linusromland@users.noreply.github.com>
2020-08-24 17:14:01 +0200

    Added unessessary switch case
    I am bored...

--- Vecka 35/Hello World/Form1.cs
+++ Vecka 35/Hello World/Form1.cs
@@ -21,7 +21,34 @@ namespace Hello_World
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            int x = 0;
+            switch (x){
+                case 0:
+                    label1.Text = "Why did you press on me?";
+                    x++;
+                    break;
+                case 1:
+                    label1.Text = "AGAIN?? WHYY SOME MEAN??";
+                    x++;
+                    break;
+                case 2:
+                    label1.Text = "I hate you!";
+                    x++;
+                    break;
+                case 3:
+                    label1.Text = "Go and die";
+                    x++;
+                    break;
+                case 4:
+                    label1.Text = "I am bored okay?";
+                    x++;
+                    break;
+                case 5:
+                    label1.Text = "Hello Niklas!";
+                    x++;
+                    break;
+            }
+            
         }
     }
 }
