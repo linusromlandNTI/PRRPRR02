51e6b54cd89659202575c1f3b0574ea1ac3477c2
linusromland <romlandlinus@gmail.com>
2020-11-09 11:47:45 +0100

    Added basic Calc Project

---
+++ Uppgifter/Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            TextBlock screen = new TextBlock();
+
+            InitializeComponent();
+            mainGrid.Margin = 11;
+            for (int i = 0; i < 5; i++)
+            {
+                mainGrid.RowDefinitions.Add(new RowDefinition());
+                mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                String btnName = "btn" + i.ToString();
+                Button btn = new Button();
+                btn.Name = btnName;
+                btn.Content = i;
+                mainGrid.Children.Add(btn);
+            }
+        }
+    }
+}
