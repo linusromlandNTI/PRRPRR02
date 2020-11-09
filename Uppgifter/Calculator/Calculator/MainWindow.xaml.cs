e73332a8bd105c21ce002b4489b68c1bd1ae2878
linusromland <romlandlinus@gmail.com>
2020-11-09 12:48:21 +0100

    "spawning"/creation of buttons in somewaht order

--- Uppgifter/Calculator/Calculator/MainWindow.xaml.cs
+++ Uppgifter/Calculator/Calculator/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calculator
 {
@@ -22,21 +11,26 @@ namespace Calculator
     {
         public MainWindow()
         {
-            TextBlock screen = new TextBlock();
+            TextBlock dsply = new TextBlock();
 
             InitializeComponent();
-            mainGrid.Margin = 11;
+
             for (int i = 0; i < 5; i++)
             {
                 mainGrid.RowDefinitions.Add(new RowDefinition());
                 mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
             }
+            
             for (int i = 0; i < 10; i++)
             {
                 String btnName = "btn" + i.ToString();
-                Button btn = new Button();
-                btn.Name = btnName;
-                btn.Content = i;
+                Button btn = new Button
+                {
+                    Name = btnName,
+                    Content = i
+                };
+                btn.SetValue(Grid.ColumnProperty, i%3);
+                btn.SetValue(Grid.RowProperty, i/3);
                 mainGrid.Children.Add(btn);
             }
         }
