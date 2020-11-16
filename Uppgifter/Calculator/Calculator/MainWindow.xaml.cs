5e20a393ea4e2d9562c7f7394f05a032dc94e975
linusromland <romlandlinus@gmail.com>
2020-11-16 12:20:17 +0100

    Cool 2D

--- Uppgifter/Calculator/Calculator/MainWindow.xaml.cs
+++ Uppgifter/Calculator/Calculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -6,10 +7,30 @@ namespace Calculator
 {
     public partial class MainWindow : Window
     {
+        //CONFIG VARIABLES
+        private int GridHeight = 5;
+        private int GridWidth = 5;
+        
         public MainWindow()
         {
             InitializeComponent();
-
+            
+            for (int i = 0; i < GridHeight; i++)
+            {
+                mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int i = 0; i < GridWidth; i++)
+            {
+                mainGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            
+            string[,] buttons = new string[,] {
+                {"", "", "", "C" },
+                {"7", "8", "9", "/"},
+                {"4", "5", "6", "*"},
+                {"1", "2", "3", "+"},
+                {".", "0", "=", "-"}
+            };
 
         }
     }
