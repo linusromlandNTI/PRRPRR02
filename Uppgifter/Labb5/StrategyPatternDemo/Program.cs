27910b678baad384491099a9c4c4d11687217d28
Linus Romland <romlandlinus@gmail.com>
2021-02-05 14:11:07 +0100

    Started with Labb5

---
+++ Uppgifter/Labb5/StrategyPatternDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using StrategyPatternDemo.Strategy;
+using System;
+
+namespace StrategyPatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("What operation would you like to perform? ('+', '-', '*', '/')");
+                var operation = Console.ReadLine();
+                double numberOne;
+                double numberTwo;
+                ICalculationStrategy strat;
+
+                Console.WriteLine("Enter the first number:");
+                numberOne = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the second number:");
+                numberTwo = Convert.ToDouble(Console.ReadLine());
+                bool reset = false;
+
+                switch (operation)
+                {
+                    case "+":
+                        strat = new AdditionStrategy();
+                        break;
+                    case "-":
+                        strat = new SubtractionStrategy();
+                        break;
+                    case "*":
+                        strat = new MultiplicationStrategy();
+                        break;
+                    case "/":
+                        strat = new DivisionStrategy();
+                        break;
+                    default:
+                        Console.WriteLine("Please select a valid operation");
+                        reset = true;
+                        strat = new AdditionStrategy();
+                        break;
+                }
+
+                if (reset)
+                {
+                    continue;
+                }
+
+                var calc = new Calculation(strat);
+                calc.CalculationInterface(numberOne, numberTwo);
+
+                Console.WriteLine("Do you wish to continue? 'y'/'n'");
+                if(Console.ReadLine().ToLower() != "y")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Uppgifter/Labb5/StrategyPatternDemo/Strategy/INameGivingStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPatternDemo
+{
+    interface INameGivingStrategy
+    {
+        void NameGivning(double numberOne, double numberTwo);
+    }
+}
